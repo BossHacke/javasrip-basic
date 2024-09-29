50abd9efa63a6208f7f9cf0886c7dcb9fc2facd1
BossHacke <vonguyenphihung123@gmail.com>
2024-09-29 12:32:30 +0700

    javasrip

---
+++ jsbasic/basic2.js
@@ -0,0 +1,20 @@
+// let user = {
+//     name: 'Hùng',
+//     age: 24,
+// };
+
+// console.log(user.name);
+
+// // let sum = (a, b) => a + b;
+// function sum(a, b) {
+//     return a + b;
+// };
+
+// console.log(`a + b = ${sum(5, 6)}`);
+
+const arr = [1, 2, 3, 4, 5];
+
+
+let mapArr = arr.reduce((value, index) => value + index);
+
+console.log(mapArr)
